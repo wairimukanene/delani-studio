30fe00da97ea2716c633489f424602d38867c375
wairimu.Kanene <mercykanene254@gmail.com>
2022-03-13 15:52:35 +0300

    commit17-added fadeToggle on the develpment and producing using jquery

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
     $("#design"). fadeToggle()
     $("#paragraph") .fadeToggle();
   });
+  $("#developing").click(function () {
+    $("#development"). fadeToggle()
+    $("#paragraph1") .fadeToggle();
+  });
+  $("#producing").click(function () {
+    $("#product"). fadeToggle()
+    $("#paragraph2") .fadeToggle();
+  });
+  $("#Images") .animate({
+    opacity: 1
+  });
 }
